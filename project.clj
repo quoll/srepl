c00656c7e9e162246cd36991cca506fd414e7bb2
Paula Gearon <gearon@gmail.com>
2024-10-17 16:53:42 -0400

    Updated to duplicate clojure.main/main

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject srepl "0.1.0-SNAPSHOT"
-  :description "A repl that provides access to sources of values defined in the repl"
-  :url "http://example.com/FIXME"
+(defproject srepl "0.0.1"
+  :description "POC for a repl that provides access to sources of values defined in the repl"
+  :url "https://github.com/quoll/srepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]]
   :aot [srepl.core]
   :main srepl.core)
--- src/srepl/core.clj
+++ src/srepl/core.clj
@@ -26,28 +26,79 @@
     (vary-meta x merge {:src f})
     x))
 
-(defn meta-eval
-  [form]
-  (let [value (eval form)]
-    (if-let [m (meta value)]
-      (if (instance? clojure.lang.IObj value)
-        (vary-meta value assoc :src form)
-        (when (var? value)
-          (alter-var-root value add-meta form)
-          (.setMeta value (merge {:src form} m))
-          value))
-      value)))
+(def eval-forms
+  "Various forms that clojure.core/eval may appear in"
+  #{'eval 'clojure.core/eval clojure.core/eval})
+
+(defn meta-eval-fn
+  [eval-fn]
+  (let [efn (or eval-fn clojure.core/eval)]
+    (fn 
+      [form]
+      (let [[value form] (if (and (seq? form) (eval-forms (first form)))
+                           (let [f (efn (second form))]
+                             [(efn f) f])
+                           [(efn form) form])]
+        (if-let [m (meta value)]
+          (if (instance? clojure.lang.IObj value)
+            (vary-meta value assoc :src form)
+            (when (var? value)
+              (alter-var-root value add-meta form)
+              (.setMeta value (merge {:src form} m))
+              value))
+          value)))))
 
 (def main-repl clojure.main/repl)
 
+(defn update-kv-list
+  [lst k vfn]
+  (-> (apply hash-map lst)
+      (update k vfn)
+      seq
+      flatten))
+
 (defn meta-repl
   [& options]
-  (apply main-repl (concat options [:eval meta-eval])))
+  (apply main-repl (update-kv-list options :eval meta-eval-fn)))
 
 (alter-var-root (var clojure.main/repl) (constantly meta-repl))
 
+(defn repl-opt
+  [[_ & args] inits]
+  (when-not (some #(= #'clojure.main/eval-opt (#'clojure.main/init-dispatch (first %))) inits)
+    (println "Clojure" (clojure-version)))
+  (meta-repl :init (fn []
+                     (#'clojure.main/initialize args inits)
+                     (apply require clojure.main/repl-requires)))
+  (prn)
+  (System/exit 0))
+
 (defn -main
-  "An alternate REPL with sources"
+  "Standalone entry point for the repl. Duplicates the standard entry by clojure.main"
   [& args]
-  (apply clojure.main/main args))
+  (try 
+    (if args
+      (loop [[opt arg & more :as args] args, inits [], flags nil]
+        (cond
+          ;; flag
+          (contains? #{"--report"} opt)
+          (recur more inits (merge flags {(subs opt 2) arg}))
+      
+          ;; init opt
+          (#'clojure.main/init-dispatch opt)
+          (recur more (conj inits [opt arg]) flags)
+  
+          :main-opt
+          (try
+            ((#'clojure.main/main-dispatch opt) args inits)
+            (catch Throwable t
+              (clojure.main/report-error t :target (get flags "report" (System/getProperty "clojure.main.report" "file")))
+              (System/exit 1)))))
+      (try
+        (repl-opt nil nil)
+        (catch Throwable t
+          (clojure.main/report-error t :target "file")
+          (System/exit 1))))
+    (finally
+      (flush))))
 
